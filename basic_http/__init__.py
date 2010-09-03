f42a5ce3b3dc1410cbef1ff4dd7978d58f1f13b9
nachopro <tranceway@gmail.com>
2010-09-03 12:36:14 -0300

    Make setup.py

---
+++ basic_http/__init__.py
@@ -0,0 +1,1 @@
+from basic_http import BasicHttp
---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='BasicHttp Lib',
+    version='0.2',
+    description='A easy to use and clean HTTP client lib for Python',
+    author='Ignacio Juan Martin Benedetti',
+    author_email='tranceway@gmail.com',
+    url='http://github.com/nachopro/basic_http',
+    packages=['basic_http'],
+)
